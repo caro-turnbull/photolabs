ea14f9678de3c7b068d84b04cda3966ef87f4084
caro-turnbull <caro.turnbull@gmail.com>
2023-09-06 18:49:18 -0400

    phototlistItem styled

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,11 +6,19 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   const photoData = props.photoData
   return (
-  <div>
-    <img src={photoData.imageSource}></img>
-    <img src={photoData.profile}></img>
-    <p>{photoData.username}</p>
-    <p>{photoData.location.city}, {photoData.location.country}</p>
+  <div className="photo-list__item">
+    <img className="photo-list__image" src={photoData.imageSource}></img>
+
+    <div className="photo-list__user-details">
+      <img className="photo-list__user-profile" src={photoData.profile}></img>
+      <div className="photo-list__user-info" >
+        {photoData.username}
+        <div className="photo-list__user-location" >
+          {photoData.location.city}, {photoData.location.country}
+        </div>
+      </div>
+      
+    </div>
   </div>
   )
 };
