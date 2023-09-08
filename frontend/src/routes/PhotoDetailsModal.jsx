9b3231eb2fd6ca94165f559f888ec28c176a1ee2
caro-turnbull <caro.turnbull@gmail.com>
2023-09-08 19:34:15 -0400

    photoList on modal working, main modal image too large, mocks editted

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 
-import '../styles/PhotoDetailsModal.scss'
+import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoList from 'components/PhotoList';
 
 const PhotoDetailsModal = (props) => {
-  const {indvPhotoData, setModalView, setIndvPhotoID, favouritesArr, setFavouritesArr} = props
-console.log("indvDATA from module", indvPhotoData)
+  const { indvPhotoData, setModalView, setIndvPhotoID, favouritesArr, setFavouritesArr } = props;
+  console.log("indvDATA from module", indvPhotoData);
 
-  return (
+  return indvPhotoData && (
     <div className="photo-details-modal">
 
       <div className="photo-details-modal__top-bar">
@@ -17,28 +17,30 @@ console.log("indvDATA from module", indvPhotoData)
         </button>
       </div>
 
-      
-        <img className="photo-details-modal--image" src={indvPhotoData.urls.full} alt="broken img"></img>
-     
+      <div> 
+        <img className="photo-details-modal--image" src= {indvPhotoData.urls.full} alt="broken img"></img>
 
-      <div className="photo-details-modal--header"> 
-        <div className="photo-details-modal__photographer-details">
-          <p>{indvPhotoData.user.name}</p>
-          <p>{indvPhotoData.location.city}, {indvPhotoData.location.country}</p>
-        </div>
-        <p>Similar Photos:</p> 
+        <p className="photo-details-modal__photographer-details">
+          By: {indvPhotoData.user.name} <br></br>
+          {indvPhotoData.location.city}, {indvPhotoData.location.country}
+        </p>
+
+        <p className="photo-details-modal--header"> 
+          Similar Photos:
+        </p>
+
+        <div className="photo-details-modal--images">
+          <PhotoList 
+            photos={indvPhotoData.similar_photos}
+            setModalView={setModalView}
+            setIndvPhotoID={setIndvPhotoID}
+            favouritesArr={favouritesArr}
+            setFavouritesArr={setFavouritesArr}
+          />
+         </div>
+
       </div>
-
-      {/* <div className="photo-details-modal--images">
-        <PhotoList 
-          photos={indvPhotoData.similar_photos}
-          setModalView={setModalView}
-          setIndvPhotoID={setIndvPhotoID}
-          favouritesArr={favouritesArr}
-          setFavouritesArr={setFavouritesArr}
-        />
-      </div> */}
-    </div>
+    </div >
   );
 };
 
